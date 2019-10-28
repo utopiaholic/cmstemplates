0be685c1f6dd9a3f680b4e4711397363c3ef4d88
Albert Reyes <albertvincentreyes@gmail.com>
2019-10-28 08:51:56 -0400

    create state
    created state object in Promote.cs

--- websites/src/components/Promote.js
+++ websites/src/components/Promote.js
@@ -3,210 +3,250 @@ import { Table, Container, Row, Col, Form, Button } from 'react-bootstrap';
 import '../styles/Promote.scss';
 
 function Promote() {
-    const [databases, setDatabases] = useState([]);
-    const [templates, setTemplates] = useState([]);
-    const [fromDatabase, setFromDatabase] = useState("");
-    const [toDatabase, setToDatabase] = useState("");
-    const [step, setStep] = useState(1);
-    
-    const [promotionList, setPromotionList] = useState([]);
+    const [promoteState, setPromoteState] = useState({
+        databases: [],
+        templates: [],
+        promotionList: [],
+        fromDatabase: "",
+        toDatabase: "",
+        step: 1,
+        fromActiveId : -1,
+        toActiveId : -1,
+    });
+
+    return (
+        <div>
+            {(promoteState.step == 1) && <Step1 promoteState={promoteState} setPromoteState={setPromoteState} />}
+            {(promoteState.step == 2) && <Step2 promoteState={promoteState} setPromoteState={setPromoteState} />}
+        </div>
+    )
+};
+
+function Step1({ promoteState, setPromoteState }) {
 
     useEffect(() => {
         fetch('http://localhost:3001/databases')
             .then(res => res.json())
             .then((data) => {
-                setDatabases(data.databases);
-                console.log(databases);
+                setPromoteState({
+                    ...promoteState,
+                    databases: data.databases,
+                });
             })
             .catch(console.log);
     }, []);
+
+    function updateFromDatabase(database, index)
+    {
+        setPromoteState({
+            ...promoteState,
+            fromDatabase: database.alias,
+            fromActiveId: index
+        });
+    }
+
+    function updateToDatabase(database, index)
+    {
+        setPromoteState({
+            ...promoteState,
+            toDatabase: database.alias,
+            toActiveId: index
+        });
+    }
+
+    return (
+        <Container className="pt-4">
+            <Row>
+                <Col>
+                    <Table bordered hover >
+                        <thead >
+                            <tr>
+                                <th>Alias</th>
+                                <th>Server</th>
+                                <th>Port</th>
+                                <th>Database name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {promoteState.databases.map((database, index) => {
+                                return (
+                                    <tr key={index} onClick={()=>{
+                                        updateFromDatabase(database, index);
+                                    }} className={promoteState.fromActiveId === index ? "selected" : "" }>
+                                        <td>{database.alias}</td>
+                                        <td>{database.server}</td>
+                                        <td>{database.port}</td>
+                                        <td>{database.databaseName}</td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </Table>
+                </Col>
+                <Col>
+                    <Table bordered hover >
+                        <thead >
+                            <tr>
+                                <th>Alias</th>
+                                <th>Server</th>
+                                <th>Port</th>
+                                <th>Database name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {promoteState.databases.map((database, index) => {
+                                return (
+                                    <tr key={index} onClick={(e)=>{
+                                        updateToDatabase(database, index);
+                                    }} className={promoteState.toActiveId === index ? "selected" : "" }>
+                                        <td>{database.alias}</td>
+                                        <td>{database.server}</td>
+                                        <td>{database.port}</td>
+                                        <td>{database.databaseName}</td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </Table>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Form >
+                        <Form.Group as={Row} controlId="fromDatabase">
+                            <Form.Label column sm="2">From:</Form.Label>
+                            <Col sm="10">
+                                <Form.Control plaintext readOnly defaultValue={promoteState.fromDatabase} />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} controlId="toDatabase">
+                            <Form.Label column sm="2">To:</Form.Label>
+                            <Col sm="10">
+                                <Form.Control plaintext readOnly defaultValue={promoteState.toDatabase} />
+                            </Col>
+                        </Form.Group>
+                        <Button style={{ float: "right" }} variant="info" onClick={() => {
+                            setPromoteState({
+                                ...promoteState,
+                                step: promoteState.step + 1
+                            });
+                        }}>
+                            Next
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+function Step2({ promoteState, setPromoteState }) {
 
     useEffect(() => {
         fetch('http://localhost:3001/templates')
             .then(res => res.json())
             .then((data) => {
-                setTemplates(data.templates);
-                console.log(templates);
+                setPromoteState({
+                    ...promoteState,
+                    templates: data.templates,
+                });
             })
             .catch(console.log);
     }, []);
 
-    function nextStep() {
-        setStep(step + 1);
-    }
-
-    function previousStep() {
-        setStep(step - 1);
-    }
-
-    function onSubmit() {
-        console.log("submitted");
-    }
-
+    
     function addToPromotionList(template) {
-        setPromotionList([...promotionList, template]);
+        setPromoteState({
+            ...promoteState,
+            promotionList : [...promoteState.promotionList, template]
+        });
     }
 
     function removeFromPromotionList(template) {
-        setPromotionList(promotionList.filter( x => x.id !== template.id ));
-    }
-
-    function step1() {
-        return (
-            <Container className="pt-4">
-                <Row>
-                    <Col>
-                        <Table bordered hover >
-                            <thead >
-                                <tr>
-                                    <th>Alias</th>
-                                    <th>Server</th>
-                                    <th>Port</th>
-                                    <th>Database name</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {databases.map((database, index) => {
-                                    return (
-                                        <tr key={index} onClick={() => setFromDatabase(database.alias)}>
-                                            <td>{database.alias}</td>
-                                            <td>{database.server}</td>
-                                            <td>{database.port}</td>
-                                            <td>{database.databaseName}</td>
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </Table>
-                    </Col>
-                    <Col>
-                        <Table bordered hover >
-                            <thead >
-                                <tr>
-                                    <th>Alias</th>
-                                    <th>Server</th>
-                                    <th>Port</th>
-                                    <th>Database name</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {databases.map((database, index) => {
-                                    return (
-                                        <tr key={index} onClick={() => setToDatabase(database.alias)}>
-                                            <td>{database.alias}</td>
-                                            <td>{database.server}</td>
-                                            <td>{database.port}</td>
-                                            <td>{database.databaseName}</td>
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </Table>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Form >
-                            <Form.Group as={Row} controlId="fromDatabase">
-                                <Form.Label column sm="2">From:</Form.Label>
-                                <Col sm="10">
-                                    <Form.Control plaintext readOnly defaultValue={fromDatabase} />
-                                </Col>
-                            </Form.Group>
-                            <Form.Group as={Row} controlId="toDatabase">
-                                <Form.Label column sm="2">To:</Form.Label>
-                                <Col sm="10">
-                                    <Form.Control plaintext readOnly defaultValue={toDatabase} />
-                                </Col>
-                            </Form.Group>
-                            <Button style={{ float: "right" }} variant="info" onClick={nextStep}>
-                                Next
-                            </Button>
-                        </Form>
-                    </Col>
-                </Row>
-            </Container>
-        );
-    }
-
-    function step2() {
-        return (
-            <Container className="pt-4">
-                <Row>
-                    <Col>
-                        <h5>Available Templates</h5>
-                        <Table bordered hover >
-                            <thead >
-                                <tr>
-                                    <th></th>
-                                    <th>Name</th>
-                                    <th>Revision Major</th>
-                                    <th>Revision Minor</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {templates.map((template, index) => {
-                                    return (
-                                        <tr key={index} onClick={() => addToPromotionList(template)}>
-                                            <td></td>
-                                            <td>{template.name}</td>
-                                            <td>{template.revision_major}</td>
-                                            <td>{template.revision_minor}</td>
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </Table>
-                    </Col>
-                    <Col>
-                        <h5>Promotion List</h5>
-                        <Table bordered hover >
-                            <thead >
-                                <tr>
-                                    <th></th>
-                                    <th>Name</th>
-                                    <th>Revision Major</th>
-                                    <th>Revision Minor</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {promotionList.map((template, index) => {
-                                    return (
-                                        <tr key={index} onClick={() => removeFromPromotionList(template)}>
-                                            <td></td>
-                                            <td>{template.name}</td>
-                                            <td>{template.revision_major}</td>
-                                            <td>{template.revision_minor}</td>
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </Table>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Form >
-                            <Button variant="dark" onClick={previousStep}>
-                                Back
-                            </Button>
-                            <Button style={{ float: "right" }} variant="info" onClick={nextStep}>
-                                Next
-                            </Button>
-                        </Form>
-                    </Col>
-                </Row>
-            </Container>
-        );
+        setPromoteState({
+            ...promoteState,
+            promotionList : promoteState.filter( x => x.id !== template.id )
+        });
     }
 
     return (
-        <div>
-            {(step == 1) && step1()}
-            {(step == 2) && step2()}
-        </div>
-    )
-};
-
+        <Container className="pt-4">
+            <Row>
+                <Col>
+                    <h5>Available Templates</h5>
+                    <Table bordered hover >
+                        <thead >
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Revision Major</th>
+                                <th>Revision Minor</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {promoteState.templates.map((template, index) => {
+                                return (
+                                    <tr key={index} onClick={() => addToPromotionList(template)}>
+                                        <td></td>
+                                        <td>{template.name}</td>
+                                        <td>{template.revision_major}</td>
+                                        <td>{template.revision_minor}</td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </Table>
+                </Col>
+                <Col>
+                    <h5>Promotion List</h5>
+                    <Table bordered hover >
+                        <thead >
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Revision Major</th>
+                                <th>Revision Minor</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {promoteState.promotionList.map((template, index) => {
+                                return (
+                                    <tr key={index} onClick={() => removeFromPromotionList(template)}>
+                                        <td></td>
+                                        <td>{template.name}</td>
+                                        <td>{template.revision_major}</td>
+                                        <td>{template.revision_minor}</td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </Table>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Form >
+                        <Button variant="dark" onClick={() => {
+                            setPromoteState({
+                                ...promoteState,
+                                step: promoteState.step - 1
+                            });
+                        }}>
+                            Back
+                        </Button>
+                        <Button style={{ float: "right" }} variant="info" onClick={() => {
+                            setPromoteState({
+                                ...promoteState,
+                                step: promoteState.step + 1
+                            });
+                        }}>
+                            Next
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
 
 export default Promote;
